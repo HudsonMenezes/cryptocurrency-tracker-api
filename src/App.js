7afc120e6edd05ed87dcfefcdfa857141f0abb20
Hudson Menezes <hudson.bm23@gmail.com>
2022-07-18 13:52:34 -0300

    API connection and setup done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from 'react'
 import './App.css'
 import axios from 'axios'
+import Coin from './Coin'
 
 function App() {
   const [coins, setCoins] = useState([])
+  const [search, setSearch] = useState('')
 
+  //API Requisition
   useEffect(() => {
     axios
       .get(
@@ -17,9 +20,43 @@ function App() {
       .catch(error => console.log(error))
   }, [])
 
+  //Get the typing on the Search Input
+  const handleChange = e => {
+    setSearch(e.target.value)
+  }
+
+  //To filter whatever we type and change to lowercase
+  const filteredCoins = coins.filter(coin =>
+    coin.name.toLowerCase().includes(search.toLowerCase())
+  )
+
   return (
-    <div className="App">
-      <h1>Hello World - I'm gonna be a Cryptocurrency Website!</h1>
+    <div className="coin-app">
+      <div className="coin-search">
+        <h1 className="coin-text">Search a Currency</h1>
+        <form>
+          <input
+            type="text"
+            placeholder="Search"
+            className="coin-input"
+            onChange={handleChange}
+          ></input>
+        </form>
+      </div>
+      {filteredCoins.map(coin => {
+        return (
+          <Coin
+            key={coin.id}
+            name={coin.name}
+            image={coin.image}
+            symbol={coin.symbol}
+            marketcap={coin.market_cap}
+            price={coin.current_price}
+            priceChange={coin.price_change_percentage_24h}
+            volume={coin.total_volume}
+          />
+        )
+      })}
     </div>
   )
 }
